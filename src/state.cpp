d30eb9aa3cb721a3296c0b0325bf2907d1e13678
Przemek Bielak <przemyslaw.bielak@protonmail.com>
2019-02-02 11:41:38 +0100

    added implementation of state constructors, getParent and getHandler

--- src/state.cpp
+++ src/state.cpp
@@ -2,19 +2,22 @@
 #include <iostream>
 
 State::State() {
-
+    /* Reserve vector size to avoid dynamic memory allocation (vector resizing). */
+    transition.reserve(MAX_NUM_OF_TRANSITIONS);
 }
 
 State::State(State *parent_, void(*handler_)()) {
-
+    parent = parent_;
+    handler = handler_;
+    transition.reserve(MAX_NUM_OF_TRANSITIONS);
 }
 
 
 State *State::getParent() {
-
+    return parent;
 }
 
 
 handler State::getHandler() {
-
+    return handler;
 }
