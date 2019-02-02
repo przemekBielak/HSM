f2d2932e26ccc3942c9b9a9b9482285966d08eec
Przemek Bielak <przemyslaw.bielak@protonmail.com>
2019-02-02 22:10:13 +0100

    added doxyfile and State class documentation

--- src/state.hpp
+++ src/state.hpp
@@ -1,3 +1,14 @@
+/**
+ * @file state.hpp
+ * @author Przemyslaw Bielak (przemyslaw.bielak@protonmail.com)
+ * @brief Describes a single state in a state machine.
+ * @version 0.1
+ * @date 2019-02-01
+ * 
+ * @copyright Copyright (c) 2019
+ * 
+ */
+
 #ifndef STATE_H
 #define STATE_H
 
@@ -6,10 +17,12 @@
 typedef void (*handler)();
 typedef struct transition_t_ transition_t;
 
-/* Configures maximum number of possible transitions for one state. */
-#define MAX_NUM_OF_TRANSITIONS   (4u)
+
+/** @brief Configures maximum number of possible transitions for one state. */
+ #define MAX_NUM_OF_TRANSITIONS   (4u)
 
 
+/** @brief Describes all possible events. */
 typedef enum event_t_ {
     EV_NO_EVENT = 0u,
     EV_STARTUP,
@@ -19,31 +32,81 @@ typedef enum event_t_ {
     NUM_OF_EVENTS
 }event_t;
 
-/* Acts as a linked list of states.
-* Each state has exactly one parent and 0-* transitions.
-*/
+
+/**
+ * @brief State is a node in linked list of states.
+ * @details Each state has exactly one parent and 0-* transitions.
+ */
 class State {
     public:
+        /**
+         * @brief Construct a new State object
+         * 
+         */
         State();
+
+        /**
+         * @brief Construct a new State object
+         * 
+         * @param parent pointer to a parent state
+         * @param handler function which is called when state is active
+         */
         State(State *parent, void (*handler)());
 
+        /**
+         * @brief Get the Parent object
+         * 
+         * @return State* 
+         */
         State *getParent();
+
+        /**
+         * @brief Get the handler function
+         * 
+         * @return handler 
+         */
         handler getHandler();
+
+        /**
+         * @brief Get the list of all transitions
+         * 
+         * @return std::vector<transition_t> 
+         */
         std::vector<transition_t> getTransition(void);
 
+        /**
+         * @brief Add new transition 
+         * 
+         * @param tr transition 
+         * @return int 
+         * @retval  0 OK
+         * @retval -1 NOK
+         */
         int addTransition(transition_t tr);
 
     private:
+        /** @brief Pointer to a parent state */
         State *parent;
+
+        /** @brief Handler function */
         void(*handler)();
-        /* Vector is used because of forward declaration of transition_t (impossible to define array this way).
-         * Size of vector is restricted to MAX_NUM_OF_TRANSITIONS, so dynamic resizing is not possible. */
+
+        /**
+         * @brief Transition list
+         * @details Vector is used because of forward declaration of transition_t (impossible to define array this way).
+         * Size of vector is restricted to MAX_NUM_OF_TRANSITIONS, so dynamic resizing is not possible.
+         * 
+         */
         std::vector<transition_t> transition;
 };
 
+/**
+ * @brief Describes state transition data
+ * 
+ */
 typedef struct transition_t_ {
-    event_t event;
-    State *nextState;
+    event_t event;      /**< Event which starts the transition */
+    State *nextState;   /**< Pointer to the next state */
 }transition_t;
 
 
